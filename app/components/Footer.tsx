3422edd0989282660c1c798256ed43b48db52d7d
AswathyRajM <aswathyraj767@gmail.com>
2026-01-29 09:52:43 +0530

    chore: display last updated date in footer

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,8 +1,14 @@
-
 export default function Footer() {
+  const lastUpdated = new Date(
+    process.env.NEXT_PUBLIC_LAST_UPDATED!,
+  ).toLocaleDateString("en-IN", {
+    year: "numeric",
+    month: "long",
+  });
   return (
     <footer className="relative p-6 border-t mt-10 md:mt-20 min-h-[5vh] flex flex-col items-center justify-center bg-bg max-w-screen overflow-x-clip ">
-      <div className="">❣️ Built with care. Designed for clarity ❣️ </div>
+      <p>❣️ Built with care. Designed for clarity ❣️ </p>
+      <p className="text-gray-400 text-sm"> Last updated: {lastUpdated}</p>
     </footer>
   );
 }
--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
+import { execSync } from "child_process";
+
+const lastUpdated = execSync("git log -1 --format=%cd --date=iso")
+  .toString()
+  .trim();
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  env: {
+    NEXT_PUBLIC_LAST_UPDATED: lastUpdated,
+  },
 };
 
 export default nextConfig;
